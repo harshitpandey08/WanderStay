6e37165e7ebb8d6c6e03e46b36831f5f85ac0359
Harshit Pandey <harshitpandey@jklu.edu.in>
2025-04-05 02:13:40 +0530

    Correcting the responsiveness

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -26,6 +26,7 @@ const nav__links = [
 const Header = () => {
 
   const headerRef = useRef(null);
+  const menuRef = useRef(null);
   const navigate = useNavigate();
 
   const {user, dispatch} = useContext(AuthContext);
@@ -51,6 +52,8 @@ const Header = () => {
     return window.removeEventListener('scroll', stickyHeaderFunc)
   });
 
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
+
   return (
     <header className="header" ref={headerRef}  >
       <Container>
@@ -64,7 +67,7 @@ const Header = () => {
             {/*================ logo end ================= */}
 
             {/*================ menu start ================= */}
-            <div className="navigation">
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
               <ul className="menu d-flex align-items-center gap-4">
                 {nav__links.map((item, index) => (
                   <li className="nav__item" key={index}>
@@ -100,10 +103,9 @@ const Header = () => {
                 )}
               </div>
 
-              <span className="mobile_menu">
+              <span className="mobile__menu" onClick={toggleMenu}>
                 <i className="ri-menu-line"></i>
               </span>
-
             </div>
           </div>
         </Row>
