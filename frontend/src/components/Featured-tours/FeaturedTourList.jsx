64b5f900e7684ce868c0eda1c325f7f8a21747ad
Harshit Pandey <harshitpandey@jklu.edu.in>
2025-04-05 02:35:45 +0530

    Correcting the home responsiveness

--- frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -6,14 +6,18 @@ import { BASE_URL } from './../../utils/config';
 
 const FeaturedTourList = () => {
 
-  const {data: featuredTours, loading , error } = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`);
+  const {
+    data: featuredTours, 
+    loading, 
+    error 
+  } = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`);
 
   return (
     <>
       { loading && <h4>Loading.........</h4>}
       { error && <h4> {error} </h4> }
       {!loading && !error && featuredTours.map(tour => (
-          <Col lg="3" className="mb-4" key={tour._id}><TourCard tour={tour} />
+          <Col lg="3" md="6" sm="6" className="mb-4" key={tour._id}><TourCard tour={tour} />
           </Col>
         ))}
     </>
