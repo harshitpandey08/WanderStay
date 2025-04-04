dfd837772c391a9f8b3e90b091a104d349cf87eb
Harshit Pandey <harshitpandey@jklu.edu.in>
2025-04-05 02:55:00 +0530

    Complete Project.

--- frontend/src/pages/ThankYou.jsx
+++ frontend/src/pages/ThankYou.jsx
@@ -7,16 +7,18 @@ const ThankYou = () => {
   return (
     <>
       <section>
-        <Row>
-          <Col lg="12" className="pt-5 text-center">
-            <div className="thank__you">
-              <span><i class="ri-checkbox-circle-line"></i></span>
-              <h1 className="mb-3 fw-semibold">Thank You</h1>
-              <h3 className="mb-4">Your tour is booked.</h3>
-            </div>
-              <Button className="btn primary__btn w-25"><Link to='/home'>Back to Home</Link></Button>
-          </Col>
-        </Row>
+        <Container>
+          <Row>
+            <Col lg="12" className="pt-5 text-center">
+              <div className="thank__you">
+                <span><i class="ri-checkbox-circle-line"></i></span>
+                <h1 className="mb-3 fw-semibold">Thank You</h1>
+                <h3 className="mb-4">Your tour is booked.</h3>
+              </div>
+                <Button className="btn primary__btn w-25"><Link to='/home'>Back to Home</Link></Button>
+            </Col>
+          </Row>
+        </Container>
       </section>
     </>
   )
